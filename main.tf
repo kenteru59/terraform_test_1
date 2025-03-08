2bea82ab0193b2f0d026f70024d4be3b9fd8cdb8
k-abe <f.kaebnet.o.cus@gmail.com>
2025-03-08 14:25:54 +0900

    Add Elastic Load Balancer

--- main.tf
+++ main.tf
@@ -4,21 +4,21 @@ provider "aws" {
 
 variable "server_port" {
   description = "HTTPサーバポート"
-  type = number
-  default = 8080
+  type        = number
+  default     = 8080
 }
 
 resource "aws_launch_template" "example" {
-    name_prefix = "terraform-template-example"
-    image_id = "ami-0a290015b99140cd1"
-    instance_type = "t2.micro"
+  name_prefix   = "terraform-template-example"
+  image_id      = "ami-0a290015b99140cd1"
+  instance_type = "t2.micro"
 
-    network_interfaces {
-        associate_public_ip_address = true 
-        security_groups = [aws_security_group.instance.id]
-    }
+  network_interfaces {
+    associate_public_ip_address = true
+    security_groups             = [aws_security_group.instance.id]
+  }
 
-    user_data = base64encode(<<-EOF
+  user_data = base64encode(<<-EOF
                 #!/bin/bash
                 echo "Hello, World4" > index.html
                 nohup busybox httpd -f -p ${var.server_port} &
@@ -27,55 +27,140 @@ resource "aws_launch_template" "example" {
                 ps aux | grep busybox > /var/log/userdata.log
                 netstat -tulnp | grep LISTEN >> /var/log/userdata.log
                 EOF
-                )
+  )
 }
 
 resource "aws_autoscaling_group" "example" {
-    launch_template {
-        id = aws_launch_template.example.id
-        version = "$Latest"
-    }
+  launch_template {
+    id      = aws_launch_template.example.id
+    version = "$Latest"
+  }
 
-    min_size = 2
-    max_size = 10
-    desired_capacity = 2
+  target_group_arns = [aws_lb_target_group.asg.arn]
+  health_check_type = "ELB"
 
-    # vpc_zone_identifier = ["subnet-0838f49bbd9a27b63"]
-    vpc_zone_identifier = data.aws_subnets.default.ids
+  min_size         = 2
+  max_size         = 10
+  desired_capacity = 2
 
-    tag {
-        key = "Name"
-        value = "terrafrom-asg-example"
-        propagate_at_launch = true
-    }
+  # vpc_zone_identifier = ["subnet-0838f49bbd9a27b63"]
+  vpc_zone_identifier = data.aws_subnets.default.ids
+
+  tag {
+    key                 = "Name"
+    value               = "terrafrom-asg-example"
+    propagate_at_launch = true
+  }
 }
 
 resource "aws_security_group" "instance" {
-    name = "terraform-example-instance"
-    vpc_id = "vpc-001b3338c91d11f0c"
+  name   = "terraform-example-instance"
+  vpc_id = "vpc-001b3338c91d11f0c"
 
-    ingress {
-        from_port = var.server_port
-        to_port = var.server_port
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  ingress {
+    from_port   = var.server_port
+    to_port     = var.server_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 data "aws_vpc" "default" {
-    filter {
-        name = "tag:Name"
-        values = ["default-vpc"]
-    }
+  filter {
+    name   = "tag:Name"
+    values = ["default-vpc"]
+  }
 }
 
 data "aws_subnets" "default" {
-    filter {
-      name = "vpc-id"
-      values = [data.aws_vpc.default.id]
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+  filter {
+    name   = "map-public-ip-on-launch"
+    values = ["true"]
+  }
+}
+
+resource "aws_lb" "example" {
+  name               = "terraform-asg-exmaple"
+  load_balancer_type = "application"
+  subnets            = data.aws_subnets.default.ids
+  security_groups    = [aws_security_group.alb.id]
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.example.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  #デフォルトはシンプル404返却
+  default_action {
+    type = "fixed-response"
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404: page not fount"
+      status_code  = 404
     }
-    filter {
-        name = "map-public-ip-on-launch"
-        values = ["true"]
+  }
+}
+
+resource "aws_security_group" "alb" {
+  name = "terraform-example-alb"
+  vpc_id = data.aws_vpc.default.id
+
+  #インバウンドhttpリクエストを許可
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  #アウトバウンドリクエストを全て許可
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_lb_target_group" "asg" {
+  name     = "terraform-asg-example"
+  port     = var.server_port
+  protocol = "HTTP"
+  vpc_id   = data.aws_vpc.default.id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 15
+    timeout             = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb_listener_rule" "asg" {
+  listener_arn = aws_lb_listener.http.arn
+  priority     = 100
+
+  condition {
+    path_pattern {
+      values = ["*"]
     }
+  }
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.asg.arn
+  }
+}
+
+output "alb_dns_name" {
+  value       = aws_lb.example.dns_name
+  description = "The domain name of the load balancer"
 }
